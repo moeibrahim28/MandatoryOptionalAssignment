653709b8a48d8c9103a3546cc7e484d514ca230b
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-07-22 08:25:47 -0400

    assignment completed

--- src/main/java/org/example/JOptional.java
+++ src/main/java/org/example/JOptional.java
@@ -1,31 +1,64 @@
 package org.example;
 
+
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class JOptional<T> {
+    private T value;
+
+
+    public JOptional(){
+        value=null;
+    }
+
+    public JOptional(T t) {
+        this.value=t;
+    }
+
     public static <T> JOptional<T> ofNullable(T t) {
-        //
+        if(t==null){
+            return new JOptional<T>();
+        }
+        else{
+            return new JOptional<T>(t);
+        }
     }
 
     public <U> JOptional<U> map(Function<T, U> mapper) {
-
+        if (value==null) {
+            return new JOptional();
+        } else {
+            return JOptional.ofNullable(mapper.apply(value));
+        }
     }
 
     public Stream<T> stream() {
+        if(value==null) {
+            return Stream.of();
+        }
+        else
+        return Stream.of(value);
 
     }
 
     public T get() {
-
+        return this.value;
     }
 
     public T orElse(T backup) {
-
+        if(value==null){
+            return backup;
+        }
+        else return this.value;
     }
 
     public T orElseGet(Supplier<T> backupCallback) {
-
+        if(value==null){
+            return backupCallback.get();
+        }
+        else return this.value;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        JOptional<String> jOptional= new JOptional<>("123");
+        JOptional<String> jOptional2= new JOptional<>();
+        System.out.println(jOptional.get());
+        System.out.println(jOptional.stream().map(value -> Integer.parseInt(value)).map(num -> num*2).findAny().get());
+        System.out.println(jOptional2.orElse("Hello"));
+        System.out.println(jOptional2.orElseGet(() -> "orElseGet"));
     }
 }
